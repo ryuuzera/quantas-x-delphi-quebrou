a7b18d3865f635cf16e347ae85a5bfdd47294f1e
Fagner Menezes <87572712+ryuuzera@users.noreply.github.com>
2022-11-17 23:29:17 -0300

    ajustes responsivo

--- pages/index.tsx
+++ pages/index.tsx
@@ -77,6 +77,9 @@ export default function Home() {
             box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
             backdrop-filter: blur(6.4px);
             -webkit-backdrop-filter: blur(6.4px);
+            display: flex;
+              justify-content: center;
+              align-items: center;
           }
         `}
       </style>
